7df3d2d99bfb57baded88c87317f7e3e6ac8b8dc
Kei Fujikawa <fujikei22funk@gmail.com>
2018-10-23 23:58:02 +0900

    WIP: CloudAnchor

---
+++ app/src/main/java/net/kboy/sceneformsample/CloudAnchorActivity.kt
@@ -0,0 +1,12 @@
+package net.kboy.sceneformsample
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class CloudAnchorActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cloud_anchor)
+    }
+}
