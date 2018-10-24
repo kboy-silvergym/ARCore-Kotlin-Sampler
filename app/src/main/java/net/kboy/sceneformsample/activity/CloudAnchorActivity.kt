0f7411c5bcd0f3391c2f4a44f1163dc5b0a6b7fe
Kei Fujikawa <fujikei22funk@gmail.com>
2018-10-24 11:37:39 +0900

    host cloud anchors.

--- app/src/main/java/net/kboy/sceneformsample/activity/CloudAnchorActivity.kt
+++ app/src/main/java/net/kboy/sceneformsample/activity/CloudAnchorActivity.kt
@@ -1,9 +1,10 @@
 package net.kboy.sceneformsample.activity
 
 import android.net.Uri
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.app.AlertDialog
+import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
 import com.google.ar.core.Anchor
 import com.google.ar.core.Plane
 import com.google.ar.sceneform.AnchorNode
@@ -16,17 +17,25 @@ import net.kboy.sceneformsample.R
 import net.kboy.sceneformsample.fragment.CloudAnchorArFragment
 
 class CloudAnchorActivity : AppCompatActivity() {
+    private enum class AnchorState {
+        NONE,
+        HOSTING,
+        HOSTED
+    }
+
     private lateinit var fragment: CloudAnchorArFragment
     private var cloudAnchor: Anchor? = null
+    private var state: AnchorState = AnchorState.NONE
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cloud_anchor)
 
-        fragment =  cloudAnchorFragment.let { it as CloudAnchorArFragment }
+        fragment = cloudAnchorFragment.let { it as CloudAnchorArFragment }
         fragment.setOnTapArPlaneListener { hitResult, plane, motionEvent ->
 
-            if (plane.type != Plane.Type.HORIZONTAL_UPWARD_FACING) {
+            if (plane.type != Plane.Type.HORIZONTAL_UPWARD_FACING ||
+                    state != AnchorState.NONE) {
                 return@setOnTapArPlaneListener
             }
 
@@ -34,11 +43,21 @@ class CloudAnchorActivity : AppCompatActivity() {
             setCloudAnchor(anchor)
 
             placeObject(fragment, cloudAnchor!!, Uri.parse("BighornSheep.sfb"))
+
+            state = AnchorState.HOSTING
+            Toast.makeText(this,
+                    "Now hosting anchor...",
+                    Toast.LENGTH_SHORT)
+                    .show()
+        }
+        fragment.arSceneView.scene.addOnUpdateListener {
+            checkUpdatedAnchor()
         }
 
         clearButton.setOnClickListener {
             setCloudAnchor(null)
         }
+        fragment.planeDiscoveryController.hide()
     }
 
     private fun setCloudAnchor(newAnchor: Anchor?) {
@@ -46,8 +65,36 @@ class CloudAnchorActivity : AppCompatActivity() {
             cloudAnchor?.detach()
         }
         cloudAnchor = newAnchor
+
+        state = AnchorState.NONE
+        Toast.makeText(this,
+                "Finish hosting anchor...",
+                Toast.LENGTH_SHORT)
+                .show()
     }
 
+    // should be synchronized method?
+    private fun checkUpdatedAnchor(){
+        if (state != AnchorState.HOSTING) {
+            return
+        }
+        val cloudAnchorState: Anchor.CloudAnchorState = cloudAnchor!!.cloudAnchorState
+
+        if (cloudAnchorState.isError) {
+            Toast.makeText(this,
+                    "Error!" + cloudAnchorState.toString(),
+                    Toast.LENGTH_SHORT)
+                    .show()
+            state = AnchorState.NONE
+        } else if (cloudAnchorState == Anchor.CloudAnchorState.SUCCESS) {
+            Toast.makeText(this,
+                    "Anchor Hosted Cloud ID" + cloudAnchor!!.cloudAnchorId,
+                    Toast.LENGTH_SHORT)
+                    .show()
+            state = AnchorState.NONE
+        }
+
+    }
 
     private fun placeObject(fragment: ArFragment, anchor: Anchor, model: Uri) {
         ModelRenderable.builder()
